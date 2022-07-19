2f3841d3ad495e62ead0b621fe8b3d1378708c39
Yelko Carvajal <ycarvajalm@baccredomatic.com>
2022-07-19 10:27:03 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// Using https://dummyjson.com/products
+// https://www.paradigmadigital.com/dev/testeo-api-rest-mocha-chai-http/
+// https://semaphoreci.com/community/tutorials/getting-started-with-node-js-and-mocha
+// https://www.chaijs.com/api/bdd/
